1c07e1566c7db40646d2da981ec82cee4595648f
Alan Shum <alan200994@gmail.com>
2018-03-03 15:33:32 -0800

    dexie indexed db for caching records

---
+++ Reviews.class.js
@@ -0,0 +1,34 @@
+class Reviews {
+  initDB() {
+    this.db = new Dexie("ShopifyReviews-Compass");
+    this.db.version(1).stores({
+      reviews: "++, [author+date], content, ratings"
+    });
+    this.db.open();
+  }
+
+  async getReviews(db) {
+    const cachedReviews = this.db.reviews;
+
+    const count = await cachedReviews.count();
+    if (count) return cachedReviews;
+
+    const reviews = await this.getAllReviews();
+    const reviewsKeys = Object.values(reviews)
+      .map(review => `${review.author}-${review.date}`);
+
+    return this.db.reviews.bulkPut(reviews, reviewsKeys)
+      .then((lastKey) => {
+        console.log(`Done adding ${reviews.length} records`);
+        return this.db.reviews;
+      }).catch(Dexie.BulkError, function (e) {
+        console.error(e);
+      });
+  }
+
+  async getAllReviews() {
+    return fetch('https://api.apify.com/v1/Ew7iFzrjYuf2jHZCY/crawlers/KFxpYZqQTrWuEnpd2/lastExec/results?token=ER5TAaboZcE8jzeug43Nrryp3&hideUrl=1&simplified=1')
+      .then(res => res.json());
+  }
+}
+
---
+++ main.js
@@ -0,0 +1,10 @@
+showReviews();
+
+async function showReviews() {
+  const review = new Reviews();
+  await review.initDB()
+  const reviewsList = await review.getReviews();
+  reviewsList.each(item => {
+    console.log(item);
+  })
+}
