53f7825cc3a76e444e40f15384019e6b3618eb47
Alan Shum <alan200994@gmail.com>
2018-04-07 15:32:26 -0700

    added scrollspy scratch

--- src/jsx/pages/Timeline.jsx
+++ src/jsx/pages/Timeline.jsx
@@ -12,35 +12,48 @@ export default class MainApp extends Component {
     };
 
     this.timeline = {
-      monthHeadings: {}
+      wrapperElement: null,
+      monthHeadingsPositions: {}
     };
   }
   render() {
     return (
       <div className="timeline">
-        <div className="timeline-wrapper">
+        <div className="timeline-wrapper" ref={ element => {this.timeline.wrapperElement = element} }>
           {
             this.state.reviews.map(review => {
-              const monthTimestamp = moment(review.date).format('MM-YYYY');
-
               const reviewCard = (
                 <div class="uk-card uk-card-default uk-card-body">
                   <div class="uk-card-badge uk-label">{ review.ratings }</div>
-                  <h3 class="uk-card-title">{ review.author } <small>{ (new Date(review.date)).toString() }</small></h3>
+                  <h3 class="uk-card-title">
+                  <a href={review.storeHref}>{ review.author }</a>
+                  <small>{ (new Date(review.date)).toString() }</small></h3>
                   <p>{ review.content }</p>
                 </div>
               );
 
-              const monthHeading = (
-                <h2>{ monthTimestamp }</h2>
-              )
-
-              if (this.timeline.monthHeadings[monthTimestamp]) {
+              const monthTimestamp = this.formatMonthHeadingTimestamp(review.date);
+              if (this.timeline.monthHeadingsPositions[monthTimestamp]) {
                 return reviewCard;
               }
 
-              this.timeline.monthHeadings[monthTimestamp] = {}
+              this.timeline.monthHeadingsPositions[monthTimestamp] = {};
+              const monthHeading = (
+                <h2 ref={ element => {this.cacheMonthHeadingPosition(monthTimestamp, element)} }>{ monthTimestamp }</h2>
+              )
               return [monthHeading, reviewCard];
+            })
+          }
+        </div>
+        <div className="timeline-scrollbar" style={ {position: 'fixed', top: 0, right: 0} }>
+          {
+            Object.entries(this.timeline.monthHeadingsPositions).map(monthHeading => {
+              const positionStyle = {
+                top: this.getMonthHeadingPositionOnScrollbar(monthHeading[1]) + '%'
+              };
+              return (
+                <div className="timeline-scrollbar-mark" style={ positionStyle }>{ monthHeading[0] }</div>
+              )
             })
           }
         </div>
@@ -56,5 +69,16 @@ export default class MainApp extends Component {
       .then(reviewItems => {
         this.setState({ reviews: reviewItems });
       })
+  }
+  formatMonthHeadingTimestamp(date) {
+    return moment(date).format('MM-YYYY');
+  }
+  cacheMonthHeadingPosition(monthTimestamp, element) {
+    this.timeline.monthHeadingsPositions[monthTimestamp] = element.getClientRects()[0].y;
+  }
+  getMonthHeadingPositionOnScrollbar(positionOnSourceWrapper) {
+    const sourceWrapperHeight = this.timeline.wrapperElement.getClientRects()[0].height;
+    console.log(positionOnSourceWrapper, sourceWrapperHeight);
+    return positionOnSourceWrapper / sourceWrapperHeight * 100;
   }
 }
