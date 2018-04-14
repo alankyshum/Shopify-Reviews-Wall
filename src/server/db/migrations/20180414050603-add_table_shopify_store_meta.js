232ac572414dd98138a18f2b27f34dad6ddbf591
Alan Shum <alan200994@gmail.com>
2018-04-13 23:32:23 -0700

    using export function + added Index for upsert capability

--- src/server/db/migrations/20180414050603-add_table_shopify_store_meta.js
+++ src/server/db/migrations/20180414050603-add_table_shopify_store_meta.js
@@ -1,29 +1,29 @@
 'use strict';
 
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('shopify_store_metas', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      store_name: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-      store_url: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-      store_description: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      }
-    })
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('shopify_store_metas');
-  }
-};
+export function up(queryInterface, Sequelize) {
+  return queryInterface.createTable('shopify_store_metas', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    store_name: {
+      allowNull: false,
+      type: Sequelize.STRING,
+      unique: true,
+    },
+    store_url: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    store_description: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    }
+  })
+}
+
+export function down(queryInterface, Sequelize) {
+  return queryInterface.dropTable('shopify_store_metas');
+}
